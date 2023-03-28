23d5bd38bf5b22fb6438e5201f4431c3bf862be0
LarisaKondratenko <122371979+LarisaKondratenko@users.noreply.github.com>
2023-03-28 22:12:48 +0500

    Update init.js

--- init.js
+++ init.js
@@ -1,3 +1,8 @@
+window.onload = function()
+{
+    const initPerson = personGenerator.getPerson();
+    document.getElementById('firstNameOutput').innerText = initPerson.firstName;
+};
 document.querySelector('#resultGenerate').addEventListener('click', () => {
     const initPerson = personGenerator.getPerson();
     document.querySelector('#genderOutput').innerText = initPerson.gender;
